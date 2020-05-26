df5b3429142e14d5f266f9e5dd46c16a1d41e089
Muni10 <sten.ivar.rokke@gmail.com>
2020-05-26 12:46:31 +0200

    Changed default host

--- go-clamav-rest/main.go
+++ go-clamav-rest/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	host       = flag.String("host", "localhost", "Address of the clamd instance")
+	host       = flag.String("host", "clamav-daemon", "Address of the clamd instance")
 	port       = flag.Int("port", 3310, "TCP port of the clamd instance")
 	listenPort = flag.Int("listenPort", 8091, "TCP port that we should listen on")
 	maxFileMem = flag.Int64("maxFileMem", 128, "Maximum memory used to store uploaded files (excess is written to disk)")
